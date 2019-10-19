c50da0001f0dbcfd7aec6328f0e3e7c085d8ae8b
nano1237 <tim.ruecker@ymail.com>
2019-10-19 03:21:40 +0200

    adding events to main export

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
 export {LoadPriorityComponent} from './components/load-priority.component';
 export {PageTransitionComponent} from './components/page-transition.component';
+export {EndEvent} from "./events/end.event";
+export {GoToEvent} from "./events/go-to.event";
+export {InnerHTMLUpdateEvent} from "./events/innerHTML-update.event";
+export {MiddleEvent} from "./events/middle.event";
+export {StartEvent} from "./events/start.event";
